447d2357576762896089a27098267da201cb3c32
umutbariscoskun <umutcoskun2017@gmail.com>
2023-08-27 00:36:35 +0300

    add checkmark Functionality

--- ToDoListSwiftUI/ViewModels/ToDoListItemViewModel.swift
+++ ToDoListSwiftUI/ViewModels/ToDoListItemViewModel.swift
@@ -5,13 +5,31 @@
 //  Created by Umut Barış Çoşkun on 23.08.2023.
 //
 
+import FirebaseAuth
+import FirebaseFirestore
 import Foundation
 
 /// This is viewModel for single todolist item
 /// each row in items list
 class ToDoListItemViewModel: ObservableObject {
     
-    func toggleIsDone(item: ToDoListItem){}
+    func toggleIsDone(item: ToDoListItem){
+        var newItem = item
+        newItem.setDone(!item.isDone)
+        
+        guard let uId = Auth.auth().currentUser?.uid else {
+            return
+        }
+        
+        let db = Firestore.firestore()
+        
+        db.collection("users")
+            .document(uId)
+            .collection("todos")
+            .document(newItem.id)
+            .setData(newItem.asDictionary())
+        
+    }
     
     init(){}
 }
